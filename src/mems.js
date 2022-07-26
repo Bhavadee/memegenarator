78d2ee70d93045baffac2d6cc0280eb423efdb98
bhavadeep reddy <bhavadeep2003@gmail.com>
2022-07-26 16:52:01 -0700

    commit

---
+++ src/mems.js
@@ -0,0 +1,32 @@
+import './App.css';
+import memesData from "./memesData.js";
+import React from "react";
+
+
+function Main() {
+    
+
+    const [memeImage, setMemeImage] = React.useState("")
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+        
+    }
+ 
+    return (
+        <main className='main'>
+            <form className="form1">
+                
+                <input className="form-input" placeholder='toptext' type = "text" />
+                <input className="form-input"  placeholder='bottomtext' type = "text" />
+                <button className="form-button"  onClick={getMemeImage} > Get a new meme image 🖼</button>
+            </form>
+        
+            <img src={memeImage} className="meme--image" alt = "bhavadeep"/>
+        </main>
+    );
+  }
+  
+  export default Main;
