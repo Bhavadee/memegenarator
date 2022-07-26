1e09e1f81a137025f5ce2df13d653230f22f527a
bhavadeep reddy <bhavadeep2003@gmail.com>
2022-07-26 16:52:22 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import Header from "./navbar";
 import './App.css';
+import Main from './mems';
+
 
 function App() {
   return (
+    <div>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Header />
       </header>
+      <Main />
+    </div>
+    
+   
     </div>
   );
 }
---
+++ src/navbar.js
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+      <div className="App">
+        <header className="App-header">
+           
+            <img  src="../troll-face.png" alt="troll" />
+  
+            <h2>meme generater</h2>
+           </header>
+           <meme />
+      </div>
+    );
+  }
+  
+  export default Header;
